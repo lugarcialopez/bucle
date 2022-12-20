9080e827ccc262aa41a31f2bb54da5b59567210f
lugarcialopez <120428904+lugarcialopez@users.noreply.github.com>
2022-12-19 19:36:43 -0500

    Primer commit
    Copiar ejercicio 3

---
+++ EJERCICIO3/js/script.js
@@ -0,0 +1,17 @@
+var resultado= 0
+function suma(a,b) {
+
+    resultado= a+b;
+document.getElementById("imprimir").innerHTML= resultado;
+
+}
+
+/*<button onclick="suma(4,6)">Sumar</button>
+
+<p id="imprimir"></p>
+
+<script src="js\script.js"></script>*/
+
+ 
+
+
